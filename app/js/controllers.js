8e14618ecb26650b4ab968aec9d4b66a8d1dbdd8
Sunil Kumar <sunilc932@gmail.com>
2014-08-16 23:42:31 -0700

    Half way through

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,12 @@
-'use strict';
+var phonecatApp = angular.module('phonecatApp', []);
 
-/* Controllers */
+function PhoneListCtrl($scope, $http) {
+  $http.get('phones/phones.json').success(function(data) {
+  	$scope.phones = data;
+  });
+}
+
+// PhoneListCtrl.$inject = ['$scope', '$http'];
+
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', PhoneListCtrl]);
+
